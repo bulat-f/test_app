c46e3183043f14353095f95e2c9121d780011bee
Fatbulat <fatbulat5@gmail.com>
2014-03-08 21:28:35 +0400

    Настроен bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = 'Test app'
+    unless page_title.blank?
+      "#{ base_title } | #{ page_title }"
+    else
+      base_title
+    end
+  end
+
 end
