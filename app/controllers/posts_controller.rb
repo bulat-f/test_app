953a0d8168a15e6223b863f43d0644cc6278e5af
Fatbulat <fatbulat5@gmail.com>
2014-03-10 15:58:59 +0400

    Организована сортировка постов по дате создания

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :admin_user,         only: [:new, :create, :edit, :update, :destroy]
+
   def index
-    @posts = Post.paginate(page: params[:page])
+    order = params[:DESC] ? :desc : :asc
+    @posts = Post.order(created_at: order).paginate(page: params[:page])
   end
 
   def show
